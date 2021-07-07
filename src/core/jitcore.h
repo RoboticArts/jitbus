9f51055114f799f6db68df4c5f41f6f6d510d33c
Robert Vasquez Zavaleta <roboticarts1@gmail.com>
2021-07-07 22:28:04 +0200

    New structure updated

--- src/core/jitcore.h
+++ src/core/jitcore.h
@@ -1,153 +1,62 @@
 #ifndef JITCORE_H
 #define JITCORE_H
 
-#include "cbuffer.h"
-#include "jitpacket.h"
-#include "jitlog.h"
-
 #ifndef JITBUS_BUFFER_SIZE
-  #define JITBUS_BUFFER_SIZE 100
+  #define JITBUS_BUFFER_SIZE 1000
 #endif
 
-#ifndef JITBUS_MIN_BUFFER_SPACE
-  #define JITBUS_MIN_BUFFER_SPACE 10
-#endif
+#include "cbuffer.h"
+#include "cobs.h"
+#include "crc.h"
+#include "jitlog.h"
+#include "jitinterface.h"
+#include "jitpacket.h"
+#include "jitstream.h"
 
-class Jitcore: public Jitlog{ 
+
+class Jitcore: public JitStream{ 
   
   public:
 
-    Jitcore(){
-      cbuffer = new CircularBuffer<uint8_t>(JITBUS_BUFFER_SIZE);
-      // _fake_read(cbuffer);
-
-      packet_available = false;
-      packet_length = 0;
-      packet_id = 0;
-
-    };
-
+    Jitcore(){};
     ~Jitcore(){
 
     };
 
-    virtual void write(uint8_t* data, int bytes_to_write){
-
-    }
-
-    virtual void read(CircularBuffer<uint8_t>* cbuffer, int bytes_to_read){
-
-    }
-
     bool available(){
-       
-        read(cbuffer, cbuffer->free_size());
-        //read(cbuffer, 200);
 
-        if (cbuffer->size() > JITBUS_MIN_BUFFER_SPACE){
-
-          JitPacket jitpacket;
-
-          if (jitpacket.findPacket(cbuffer) == true){
-
-            packet_available = true;
-            packet_length = jitpacket.packetLength();
-            packet_id = jitpacket.packetId();
-          }
-
-          else{
-
-            packet_available = false;
-            packet_length = 0;
-            packet_id = 0;
-
-          }
-        }
-
-        else{
-          packet_available = false;
-        }
-
-        return packet_available;
+        return JitStream::availablePacket();
     }
 
     template<typename Type>
     bool receivePacket(const Type& data, uint16_t data_id){
-
-      bool is_received = false;
-
-        if (packet_available == true){
-
-          if (data_id == packet_id){
-
-            JitPacket jitpacket;
-            jitpacket.parsePacket(data, cbuffer, packet_length);
-            packet_available = false;
-            is_received = true;
-          }
-
-        }
-
-      return is_received;
+   
+        return JitStream::readPacket(data, data_id);
     }
 
     template<typename Type>
     void sendPacket(const Type& data, uint16_t data_id){
-        
-        JitPacket jitpacket;
-        jitpacket.buildPacket(data, data_id);
 
-        write(jitpacket.buffer, jitpacket.packetLength());
+        JitStream::writePacket(data, data_id);
 
     }
 
+    template<typename Type>
+    void sendPacketHz(const Type& data, uint16_t data_id, uint32_t& time, float frequency){
+
+        JitStream::writePacketHz(data, data_id, time, frequency);
+
+    }
+
+    template<typename Type>
+    void sendPacketBlocking(const Type& data, uint16_t data_id){
+
+        JitStream::writePacketBlocking(data, data_id);
+    }
 
 
   private:
 
-    CircularBuffer<uint8_t>* cbuffer;
-    bool packet_available;
-    int packet_length;
-    int packet_id;
-
-    void _fake_read(CircularBuffer<uint8_t>* cbuffer){
-
-      packet_available = true;
-      packet_id = 66;
-      
-      //garbage
-      //cbuffer->push(215);
-      //cbuffer->push(176);
-      //cbuffer->push(51);
-      //start frame
-      cbuffer->push(255);
-      cbuffer->push(255);
-      //total length
-      cbuffer->push(0);
-      cbuffer->push(0);
-      cbuffer->push(0);
-      cbuffer->push(18);
-      //id
-      cbuffer->push(0);
-      cbuffer->push(66);
-      //data
-      cbuffer->push(128);
-      cbuffer->push(62);
-      cbuffer->push(17);
-      cbuffer->push(0);
-      cbuffer->push(87);
-      cbuffer->push(0);
-      //crc
-      cbuffer->push(0);
-      cbuffer->push(0);
-      //end frame
-      cbuffer->push(254);
-      cbuffer->push(127);
-      //garbage
-      //cbuffer->push(111);
-      //cbuffer->push(222);
-
-    }
 
 };
 
--- src/core/jitpacket.h
+++ src/core/jitpacket.h
@@ -8,325 +8,156 @@
 *   ||    0xFFFF   |     ---      |     ---       ||     ---     ||   ---     |   0xFE7F  ||
 */    
 
-#define PROTOCOL_START_FRAME 0xFFFF
-#define PROTOCOL_END_FRAME 0xFE7F
-#define PROTOCOL_HEADER_LENGTH 8
-#define PROTOCOL_TAIL_LENGTH 4 
+#define PROTOCOL_START_FRAME 0xAF
+#define PROTOCOL_END_FRAME 0xFE
+#define PROTOCOL_DATA_ID_LENGTH uint32_t(sizeof(uint16_t))
+#define PROTOCOL_CRC_LENGTH uint32_t(sizeof(uint16_t))
 
-
-class JitPacket{ 
+class JitPacket: public JitInterface { 
 
     public:
 
-        uint8_t buffer[100];
         JitPacket(){};
+
         ~JitPacket(){};
     
         // --------------- BUILD PACKET  ---------------- //
 
         template<typename Type>
-        void buildPacket(const Type& data, uint16_t data_id){
-
+        uint32_t buildPacket(uint8_t* buffer, const Type& data, uint16_t data_id){
+            
+            uint32_t cobs_length;
             uint32_t data_length = sizeof(Type);
-            buildHeader(data_id, data_length);
-            buildData(data, data_length);
-            buildTail(data_length);
-
-        };
-
-        void buildHeader(uint16_t data_id, uint32_t data_length){
-            
-            // Start frame
-            uint16_t start_frame = PROTOCOL_START_FRAME;
-            buffer[0] = start_frame >> 8 & 0xFF;
-            buffer[1] = start_frame & 0xFF;
-            
-            // Total length
-            uint32_t total_length = PROTOCOL_HEADER_LENGTH + data_length + PROTOCOL_TAIL_LENGTH;
-            this->total_length = total_length;
-            buffer[2] = total_length >> 24 & 0xFF;
-            buffer[3] = total_length >> 16 & 0xFF;
-            buffer[4] = total_length >> 8 & 0xFF;
-            buffer[5] = total_length & 0xFF;
+            uint32_t packet_length = PROTOCOL_DATA_ID_LENGTH + data_length + PROTOCOL_CRC_LENGTH;
 
             // Data id
-            buffer[6] = data_id >> 8 & 0xFF;
-            buffer[7] = data_id & 0xFF;
-            
-
-        }
-
-        template<typename Type>
-        void buildData(const Type& data,  uint32_t data_length){
+            buffer[0] = data_id >> 8 & 0xFF;
+            buffer[1] = data_id & 0xFF;
 
             // Data
             uint8_t* data_ptr = (uint8_t*)&data;
 
-            for (uint32_t i = PROTOCOL_HEADER_LENGTH; i < PROTOCOL_HEADER_LENGTH + data_length; i++){
+            for (uint32_t i = PROTOCOL_DATA_ID_LENGTH; i < 2 + data_length; i++){
                 buffer[i] = *data_ptr;
                 data_ptr++;
             }
 
-        }
-
-        void buildTail(uint32_t data_length){
-
             // CRC
-            uint16_t crc = calculate_crc(data_length);
-            buffer[PROTOCOL_HEADER_LENGTH + data_length + 0] = crc >> 8 & 0xFF;
-            buffer[PROTOCOL_HEADER_LENGTH + data_length + 1] = crc & 0xFF;
+            uint16_t crc = CRC::compute_crc16(buffer, packet_length);
+            buffer[PROTOCOL_DATA_ID_LENGTH + data_length + 0] = crc >> 8 & 0xFF;
+            buffer[PROTOCOL_DATA_ID_LENGTH + data_length + 1] = crc & 0xFF;
 
-            // End frame
-            uint16_t end_frame = PROTOCOL_END_FRAME;
-            buffer[PROTOCOL_HEADER_LENGTH + data_length + 2] = end_frame >> 8 & 0xFF;
-            buffer[PROTOCOL_HEADER_LENGTH + data_length + 3] = end_frame & 0xFF;
-
-        }
-
-        int calculate_crc(uint32_t data_length){
-
-            return 0;
-        }
-        
-
-        // --------------- FIND PACKET  ---------------- //
-
-        bool findPacket(CircularBuffer<uint8_t>* cbuffer){
+            cobs_length = encodePacket(buffer, packet_length);
             
-            bool packet_found = false;
-            int find = START_FRAME;
-  /*          
-            for(int i = 0; i<cbuffer->size(); i++){
-                Serial.println(cbuffer->index(i));
+            return cobs_length;
+        };
+
+
+        uint32_t encodePacket(uint8_t* buffer, uint32_t buffer_length){
+
+            uint8_t buffer_aux[JITBUS_BUFFER_SIZE];
+            uint32_t total_bytes, total_bytes_aux;
+
+            total_bytes_aux = COBS::encode(buffer, buffer_length, buffer_aux, PROTOCOL_START_FRAME);
+            total_bytes = COBS::encode(buffer_aux, total_bytes_aux, buffer, PROTOCOL_END_FRAME);
+        
+            return total_bytes;
+        }
+
+
+        // --------------- PROCESS PACKET ---------------- //
+
+        bool processPacket(uint8_t* buffer, uint32_t& buffer_length){
+            
+            bool packet_status = false;
+/*
+            for(uint32_t i = 0; i<buffer_length; i++){
+
+                print_debug("%X", buffer[i]);
             }
 */
-            //printf("%d\n", cbuffer->size() );
-            while(cbuffer->size() > 0 && packet_found == false){
+            buffer_length = decodePacket(buffer, buffer_length);
 
-                switch(find){
+            if (buffer_length > 0){
 
-                    case START_FRAME: 
-                        
-                        if (findStartFrame(cbuffer) == true){
+                if (CRC::check_crc16(buffer, buffer_length) == true){
+
+                    packet_status = true;
+                    print_debug("jitpacket::processPacket -> CRC ok");
                     
-                            find = LENGTH_FRAME;
-
-                        }
-
-                        else{
-
-                            find = START_FRAME;
-
-                        }
-                        
-                        break;
-
-                    case LENGTH_FRAME:
-
-                        if (findLengthFrame(cbuffer) == true){
-
-                            find = END_FRAME;
-
-                        }
-                        
-                        else{
-
-                            find = START_FRAME;
-
-                        }
-
-                        break;
-
-                    case END_FRAME:
-
-                        if (findEndFrame(cbuffer) == true){
-
-                            find = CRC_FRAME;
-
-                        }
-
-                        else{
-
-                            find = START_FRAME;
-
-                        }
-
-                        break;
-
-                    case CRC_FRAME:
-                        
-                        if (findCrcFrame(cbuffer) == true){
-
-                            packet_found = true;
-
-                        }
-
-                        else{
-
-                            find = START_FRAME;
-
-                        }
-
-                        break;
-
-                };
-
+                }
+                else{
+                    packet_status = false;
+                    print_debug("jitpacket::processPacket -> CRC failed");
+                }            
+            } 
+            else{
+                packet_status = false;
+                print_debug("jitpacket::processPacket -> Decode failed because length does not match data, malformed");
+                   
             }   
 
-            return packet_found;
+            return packet_status;
 
         }
 
-        bool findStartFrame(CircularBuffer<uint8_t>* cbuffer){
-
-            bool found = false;
-
-            uint16_t start_frame = 0;
-            start_frame = start_frame | cbuffer->index(0) << 8;
-            start_frame = start_frame | cbuffer->index(1) << 0;
-
-            if (start_frame == PROTOCOL_START_FRAME){
-
-                found = true;
-            }
-            else{
-                found = false;
-                cbuffer->pop();
-                //printf("start frame failed\n");
-            }
-
-            return found;
-        }
-
-        bool findLengthFrame(CircularBuffer<uint8_t>* cbuffer){
-
-            bool found = false;
-
-            uint32_t total_length = 0;
-            total_length = total_length | cbuffer->index(2 + 0) << 24;  
-            total_length = total_length | cbuffer->index(2 + 1) << 16;    
-            total_length = total_length | cbuffer->index(2 + 2) << 8;    
-            total_length = total_length | cbuffer->index(2 + 3) << 0; 
-
-            if (total_length <= cbuffer->free_size() && total_length > PROTOCOL_HEADER_LENGTH + PROTOCOL_TAIL_LENGTH){
-                found = true;
-                this->total_length = total_length;
-            }
-            else{
-                found = false;
-                cbuffer->pop();
-                printf("length frame failed\n");
-            }
-
-            return found;
-        }
-
-        bool findEndFrame(CircularBuffer<uint8_t>* cbuffer){
-
-            bool found = false;
-           
-            uint16_t end_frame = 0;
-            end_frame = end_frame | cbuffer->index((this->total_length-1) - 1) << 8;
-            end_frame = end_frame | cbuffer->index((this->total_length-1) - 0) << 0;
-
-            if (end_frame == PROTOCOL_END_FRAME){
-
-                found = true;
-            }
-            else{
-                found = false;
-                cbuffer->pop();
-                printf("end frame failed\n");
-            }
-
-            return found;
-        }
-
-        bool findCrcFrame(CircularBuffer<uint8_t>* cbuffer){
-            
-            bool found = false;
-
-            int data_from = PROTOCOL_HEADER_LENGTH;
-            int data_to = data_from + (this->total_length - PROTOCOL_TAIL_LENGTH);
-            int crc_from =  (this->total_length - PROTOCOL_TAIL_LENGTH) + 1;
-            int crc_to = crc_from + 2; 
-
-            if (check_crc16_from_cbuffer(cbuffer, data_from, data_to, crc_from, crc_to) == true){
-
-                found = true;
-                this->id = this->id | cbuffer->index(6) << 8;
-                this->id = this->id | cbuffer->index(7) << 0;
-                //printf("frame OK\n");
-            }
-            else{
-                found = false;
-                cbuffer->pop();
-                //printf("crc frame failed\n");
-            }
-            
-            found = true; //  Remove when CRC is implemented! <------------------------------------------------------
-            return found;
-
-        }
-
-
-        bool check_crc16_from_cbuffer(CircularBuffer<uint8_t>* cbuffer, int data_from, int data_to, int crc_from, int crc_to){
+        bool check_crc16(uint8_t* buffer, uint32_t buffer_length){
 
             return true;
         }
 
+        uint32_t decodePacket(uint8_t* buffer, uint32_t buffer_length){
 
-        uint32_t packetLength(){
+            uint8_t buffer_aux[JITBUS_BUFFER_SIZE];
+            uint32_t total_bytes = 0, total_bytes_aux;
 
-            return this->total_length;
+            total_bytes_aux = COBS::decode(buffer, buffer_length, buffer_aux,  PROTOCOL_END_FRAME);
+            total_bytes = COBS::decode(buffer_aux, total_bytes_aux, buffer,  PROTOCOL_START_FRAME);
+
+            return total_bytes;
         }
 
-        int packetId(){
+        // --------------------- OTHERS -------------------- //
 
-            return this->id;
+        uint8_t startFrame(){
+
+            return PROTOCOL_START_FRAME;
+        }
+
+        uint8_t endFrame(){
+
+            return PROTOCOL_END_FRAME;
+        }
+
+        uint16_t packetId(const uint8_t* buffer){
+
+            uint16_t packet_id = 0;
+
+            packet_id = packet_id | buffer[1] << 0;
+            packet_id = packet_id | buffer[0] << 8;
+
+            return packet_id;
         }
 
         // --------------- PARSE PACKET  ---------------- //
 
         template<typename Type>
-        void parsePacket(const Type& data, CircularBuffer<uint8_t>* cbuffer, int packet_length){
-            
-            int received_data_length = packet_length - PROTOCOL_HEADER_LENGTH - PROTOCOL_TAIL_LENGTH;
-            int current_data_length = sizeof(Type);
-
-            int data_from = PROTOCOL_HEADER_LENGTH;
-            int data_to;
-
-            if(received_data_length > current_data_length){
-                
-                data_to = data_from + current_data_length - 1;
-
-            }
-
-            else{
-
-                data_to = data_from + received_data_length - 1;
-            }
-
+        void parsePacket(const Type& data, uint8_t* buffer, uint32_t buffer_length){
+    
             uint8_t* data_ptr = (uint8_t*)&data;
 
-            for (int i = data_from; i <= data_to; i++){
+            for (uint32_t i = PROTOCOL_DATA_ID_LENGTH; i < buffer_length-PROTOCOL_CRC_LENGTH; i++){
                
-                *data_ptr = cbuffer->index(i);
+                *data_ptr = buffer[i];
                 data_ptr++;
             }
-
-            cbuffer->pop_until_index(packet_length);
+   
         }
 
 
     private:
 
-        uint32_t total_length = 0;
-        int id = 0;
-
-        enum process {START_FRAME, LENGTH_FRAME, ID_FRAME, DATA_FRAME, CRC_FRAME, END_FRAME};
-               
+      
 
 };
 
